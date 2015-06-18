c45933d510ebbe3e3ba07337a37a6bc675732545
Robert O'Rourke <robert@o-rourke.org>
2015-06-18 15:24:01 +0100

    update to latest

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-login-shortcodes
+Login Shortcodes
 ================
 
 Allows you to put a stylable WP login form anywhere on your site as well as shortcode buttons for showing/hiding content to logged in or logged out users.
--- js/admin.js
+++ js/admin.js
@@ -42,35 +42,6 @@
 
 		} );
 
-		// tinymce popup
-		$( '#logged-in-popup' ).on( 'click', '#logged-in-update input', function( e ) {
-			e.preventDefault();
-
-			var ed = tinyMCE.activeEditor,
-				plugin = ed.plugins.loggedinout,
-				roles = [],
-				checks = $( '#logged-in-popup input[type="checkbox"]' ),
-				attr = '';
-
-			tinyMCEPopup.close();
-
-			checks.each( function() {
-				if ( $( this ).is( ':checked' ) )
-					roles.push( $( this ).val() );
-			} );
-
-			if ( checks.length != roles.length )
-				attr = ' role="' + roles.join( ',' ) + '"';
-
-			plugin.insertShortcode( 'loggedin', attr );
-		} );
-		
-		$( '#logged-in-popup' ).on( 'click', '#logged-in-cancel a', function( e ) {
-			e.preventDefault();
-			tinyMCEPopup.close();
-		} );
-		
-
 	} );
 
 } )( jQuery );
